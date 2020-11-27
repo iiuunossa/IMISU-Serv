484737a93f253ce62eb7ad6fc43d43d997bb509d
iiuunossa <karabuning_27@hotmail.com>
2020-11-27 15:39:55 +0700

    Create first page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>IMISU-Service</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #ccfff5;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+                color: #333333;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #111111;
+                background-color: #88DDBB;
+                padding: 0 25px;
+                font-size: 15px;
+                font-weight: 600;
+                letter-spacing: .4rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           
+
+            <div class="content">
+                <div class="title m-b-md">
+                    IMISU-SerVice
+                </div>
+                <div class="links m-b-md">
+                    <a href="">Start Service Here</a>
+                </div>
+                
+
+                @if (Route::has('login'))
+                <div class="top-rtight links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}"><span>Login</span></a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            </div>
+        </div>
+
+            </div>
+        </div>
+    </body>
+</html>
